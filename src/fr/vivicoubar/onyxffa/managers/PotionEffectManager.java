f0150ff70f1299b660fbcab46c29088e8be1264a
ViViCouBar <40126535+Vivicoubar@users.noreply.github.com>
2021-04-13 19:00:39 +0200

    test

--- src/fr/vivicoubar/onyxffa/managers/PotionEffectManager.java
+++ src/fr/vivicoubar/onyxffa/managers/PotionEffectManager.java
@@ -1,6 +1,7 @@
 package fr.vivicoubar.onyxffa.managers;
 
 import fr.vivicoubar.onyxffa.utils.FFaPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -9,9 +10,10 @@ public class PotionEffectManager {
 
     public void addPotionEffect(Player player, PotionEffect potionEffect){
         for(PotionEffect activepotion : player.getActivePotionEffects()){
+            Bukkit.broadcastMessage(activepotion.getType().getName());
             if(potionEffect.getType() == activepotion.getType()){
                 int time = activepotion.getDuration() + potionEffect.getDuration();
-                player.addPotionEffect(new PotionEffect(activepotion.getType(), time, activepotion.getAmplifier()));
+                player.addPotionEffect(new PotionEffect(potionEffect.getType(), time, potionEffect.getAmplifier()));
                 return;
             }
         }
