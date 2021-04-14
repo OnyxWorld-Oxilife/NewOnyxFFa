873aae2e9638f49cd5a2cdb1e8e729b03483055b
SnowIce <mc.snowice@gmail.com>
2021-04-14 18:11:42 +0200

    Added timeParser

--- src/fr/vivicoubar/onyxffa/events/wanted/WantedEvent.java
+++ src/fr/vivicoubar/onyxffa/events/wanted/WantedEvent.java
@@ -65,6 +65,20 @@ public class WantedEvent {
         setTarget(newTarget);
     }
 
+    public String timeParser(int seconds) {
+        double d = Math.floor(seconds/(3600*24));
+        double h = Math.floor(seconds % (3600*24) / 3600);
+        double m = Math.floor(seconds % 3600 / 60);
+        double s = Math.floor(seconds % 60);
+
+        String dDisplay = d > 0 ? d + "d " : "";
+        String hDisplay = h > 0 ? h + "h " : "";
+        String mDisplay = m > 0 ? m + "m " : "";
+        String sDisplay = s > 0 ? s + "s" : "";
+
+        return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
+
     public void setTarget(FFaPlayer newTarget) {
         if(newTarget.isInArena()) {
             target = newTarget;
@@ -83,7 +97,7 @@ public class WantedEvent {
                         setRandomTarget();
                     }
                     if(timer2 > 0) {
-                        Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + (timer2) + "§e sec!");
+                        Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + timeParser(timer2) + " §e!");
                     }
                     timer2--;
                 }
@@ -114,7 +128,7 @@ public class WantedEvent {
                     cancel();
                 }
                 if (timer == 30 || timer == 15 || timer == 10 || timer == 3 || timer == 2 || timer == 1) {
-                    Bukkit.broadcastMessage("§b[§eOnyxFFa§b] L'event Wanted va commencer dans §c" + timer + "sec !");
+                    Bukkit.broadcastMessage("§b[§eOnyxFFa§b] L'event Wanted va commencer dans §c" + timeParser(timer) + " !");
                 } else if (timer <= 0) {
                     cancel();
                     Bukkit.broadcastMessage("§b[§eOnyxFFa§b] L'event Wanted commence!");
@@ -126,7 +140,7 @@ public class WantedEvent {
                     }
 
                     timer = 310;
-                    Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + (timer-300) + "§e sec!");
+                    Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + timeParser(timer-300) + " §e!");
                     new BukkitRunnable() {
                         @Override
                         public void run() {
@@ -134,13 +148,13 @@ public class WantedEvent {
                                 cancel();
                             }
                             if(timer == 301 || timer == 302 || timer == 303 || timer == 305){
-                                Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + (timer-300) + " §e sec!");
+                                Bukkit.broadcastMessage("§e[§cWanted§e] La cible sera désignée dans §c" + timeParser(timer-300) + "  §e!");
                             }
                             if(timer == 300){
                                 setRandomTarget();
                             }
                             if (timer == 300|| timer == 60 || timer == 30 || timer == 15 || timer == 10 || timer == 3 || timer == 2 || timer == 1) {
-                                Bukkit.broadcastMessage("§e[§cWanted§e] La cible est §c" + target.getPlayer().getName() + "§e! Il reste "+ timer + "sec!");
+                                Bukkit.broadcastMessage("§e[§cWanted§e] La cible est §c" + target.getPlayer().getName() + "§e! Il reste "+ timeParser(timer) + " !");
                             } else if (timer <= 0) {
                                 state = EventState.STOPPING;
                                 cancel();
